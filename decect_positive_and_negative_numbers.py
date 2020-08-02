2f6de2eeae0be94eef0c0bf3ab4151d9d167f39c
moundu <munenemuondu@gmail.com>
2020-08-02 11:31:50 +0300

    This site checks for positive and negative numbers

---
+++ decect_positive_and_negative_numbers.py
@@ -0,0 +1,9 @@
+x = float(input("PLS insert a number:  "))
+
+
+if x > 0:
+    print("This is a Positive number")
+elif x < 0:
+    print("This is a negative number")
+else:
+    print("The number is zero")
